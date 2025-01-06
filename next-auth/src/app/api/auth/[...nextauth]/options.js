e13195f0a8bcbe6998b2d09b8d5088b4faff7d94
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-06 11:59:39 +0600

    have to complete dave gray course

--- next-auth/src/app/api/auth/[...nextauth]/options.js
+++ next-auth/src/app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,42 @@
 import { NextAuthOptions } from "next-auth";
-export const NextAuthOptions = {
-  providers: [],
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "User Name:",
+          type: "text",
+          placeholder: "Your Name",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your awesome password",
+        },
+      },
+      async authorize(credentials) {
+        // This is where you need to retrive user data
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: "42", name: "Arif", password: "nextauth" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };
