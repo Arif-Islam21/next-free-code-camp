33d76dfb93a2e2e3c75ec7b38cf8991453b186ea
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-08 16:51:40 +0600

    custom theme added

--- car-doctor-with-next-js/car-doctor-pro-with-next/src/app/page.js
+++ car-doctor-with-next-js/car-doctor-pro-with-next/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello car doctor pro</div>;
+  return <div className="bg-primary">Hello car doctor pro</div>;
 }
--- car-doctor-with-next-js/car-doctor-pro-with-next/tailwind.config.mjs
+++ car-doctor-with-next-js/car-doctor-pro-with-next/tailwind.config.mjs
@@ -16,6 +16,21 @@ const tailwindConfig = {
     },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        carDoctor: {
+          primary: "#FF3911",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
 
 export default tailwindConfig;
