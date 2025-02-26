681c363269c4453d1c53ec90ef5219d2a589d8b6
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-02-26 09:36:14 +0600

    tailwind is not working

---
+++ code-evalution-next/hello-next/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ForgotPass = () => {
+  return <div>Forgot password</div>;
+};
+
+export default ForgotPass;
---
+++ code-evalution-next/hello-next/src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const privateRoute = () => {
+  return <div>This is a private route</div>;
+};
+
+export default privateRoute;
