86d9e65f57ddf247dcfb5b4c5e8fd54a3180d0d0
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-02-28 21:12:02 +0600

    learned about loading

--- code-evalution-next/hello-next/src/app/blog/page.tsx
+++ code-evalution-next/hello-next/src/app/blog/page.tsx
@@ -2,7 +2,12 @@ export const metadata = {
   title: "About Arif",
 };
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("BlogPage loading...");
+    }, 1000); // Simulating async data fetching
+  });
   return <div>this is BlogPage</div>;
 };
 
