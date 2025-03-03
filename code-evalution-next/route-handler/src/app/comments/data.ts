864cf14b15d6d79ea15a457719cfac8560323b0e
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-03-03 16:45:34 +0600

    error in post request

---
+++ code-evalution-next/route-handler/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    content: "First comment",
+  },
+  {
+    id: 2,
+    content: "Second comment",
+  },
+  {
+    id: 3,
+    content: "Third comment",
+  },
+];
