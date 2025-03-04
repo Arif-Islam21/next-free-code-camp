b377aec8875a0b1a0b34a437a163e45492059570
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-03-04 16:19:47 +0600

    know about redirects

--- code-evalution-next/route-handler/src/app/profile/api/route.ts
+++ code-evalution-next/route-handler/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -6,9 +6,17 @@ export async function GET(request: NextRequest) {
   // console.log(requestHeaders.get("Authorization"));
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("result", "arif");
+  console.log(cookieStore.get("result"));
+
   return new Response("<h1>Hello profile!</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
