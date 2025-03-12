34c1321c02c3dba53b57c8c3a3449cd4a2e3bd57
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-03-12 21:52:33 +0600

    learning client only code

--- code-evalution-next/rendering-demo/src/app/client-route/page.tsx
+++ code-evalution-next/rendering-demo/src/app/client-route/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { serverSideFunction } from "@/utils/server-utils";
+// import { serverSideFunction } from "@/utils/server-utils";
+import { useTheme } from "../components/theme-provider";
 
 const ClientRoute = () => {
-  const result = serverSideFunction();
-  return <div>Client Route{result}</div>;
+  const theme = useTheme();
+
+  return <h1 style={{ color: theme.colors.primary }}>Client Route Page</h1>;
 };
 
 export default ClientRoute;
