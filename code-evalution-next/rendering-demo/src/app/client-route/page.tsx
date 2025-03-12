9c64982419dcb4fd093584b864ce45881fcd2cda
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-03-12 22:01:35 +0600

    installed client only by npm

--- code-evalution-next/rendering-demo/src/app/client-route/page.tsx
+++ code-evalution-next/rendering-demo/src/app/client-route/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { clientSideFunction } from "@/utils/client-utils";
 // import { serverSideFunction } from "@/utils/server-utils";
 import { useTheme } from "../components/theme-provider";
 
 const ClientRoute = () => {
   const theme = useTheme();
+  const result = clientSideFunction();
 
-  return <h1 style={{ color: theme.colors.primary }}>Client Route Page</h1>;
+  return (
+    <>
+      <h1 style={{ color: theme.colors.primary }}>Client Route Page</h1>
+      <p>{result}</p>
+    </>
+  );
 };
 
 export default ClientRoute;
--- code-evalution-next/rendering-demo/src/app/server-route/page.tsx
+++ code-evalution-next/rendering-demo/src/app/server-route/page.tsx
@@ -1,9 +1,11 @@
+// import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils";
 import React from "react";
 
 const ServerRoutePage = () => {
   const result = serverSideFunction();
-  return <div>Server route {result}</div>;
+  // const clientResult = clientSideFunction();
+  return <div>Server route {result} ...</div>;
 };
 
 export default ServerRoutePage;
